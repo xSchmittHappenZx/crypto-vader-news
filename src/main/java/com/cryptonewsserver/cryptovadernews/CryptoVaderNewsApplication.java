8f70092f221b632924230caaaded86d9e4bfd024
Caleb Schmitt <cschmitt@shelterinsurance.com>
2018-07-24 15:05:06 -0500

    Back-end: spring init completed, will pick up more soon

---
+++ src/main/java/com/cryptonewsserver/cryptovadernews/CryptoVaderNewsApplication.java
@@ -0,0 +1,12 @@
+package com.cryptonewsserver.cryptovadernews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoVaderNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoVaderNewsApplication.class, args);
+	}
+}
